57cd67aec3b22c1d34abdd1b46e17c735edbe0e9
Muntasir Mubin Nashit <nashit.1999@gmail.com>
2022-01-04 23:23:10 +0600

    Jan 4, 22

--- Sum of Round Numbers.py
+++ Sum of Round Numbers.py
@@ -2,10 +2,10 @@ n=int(input())
 for i in range(0,n):
     num=input()
     l=len(num)
-    listofnum=[]
+    listofSplitNum=[]
     for j in range(0,l):
         if num[j]!='0':
             splitnum=num[j]+("0"*(l-j-1))
-            listofnum.append(splitnum)
-    print(len(listofnum))
-    print(" ".join(listofnum))
+            listofSplitNum.append(splitnum)
+    print(len(listofSplitNum))
+    print(" ".join(listofSplitNum))
